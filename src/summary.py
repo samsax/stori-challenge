913fe5fd337beb644f9f7d73c2c71d74d015449a
samsax <samuelsaxofon@gmail.com>
2021-10-09 18:23:45 -0500

    Refactor for lambda function

---
+++ src/summary.py
@@ -0,0 +1,7 @@
+class Summary():
+    def __init__(self,months_transacctions, balance, credit, debit):
+        self.months_transacctions = months_transacctions
+        self.balance = balance
+        self.credit = credit
+        self.debit = debit
+        self.transaction = 0
