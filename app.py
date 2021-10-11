1baa6e37d995a0d6f64234b013fe9fe2010a0724
samsax <samuelsaxofon@gmail.com>
2021-10-10 21:17:35 -0500

    logs to debug lambda function and db connection

--- app.py
+++ app.py
@@ -2,13 +2,22 @@ import csv
 from function.account import get_or_create_account
 from function.aws_services import AwsServices
 from function.transaction import send_mail_summary, summary_transacction
-
+import os
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def lambda_handler(event, _):
+    logger.info('## ENVIRONMENT VARIABLES')
+    logger.info(os.environ)
+    logger.info('## EVENT')
+    logger.info(event)
     awsServices = AwsServices(event)
     awsFile = awsServices.get_s3_file()
+    logger.info(awsFile)
     new_person = get_or_create_account()
     input_file = csv.DictReader(awsFile.file)
+    logger.info(input_file)
     summary = summary_transacction(input_file, new_person)
     send_mail_summary(summary)
     return { 
--- function/account.py
+++ function/account.py
@@ -1,8 +1,11 @@
 from function.models import db, Account 
-
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 def get_or_create_account():
     if(not db.is_connection_usable):
         db.connect()
+        logger.info('## DB Connect')
     db.create_tables([Account])
     new_account = Account.create(name='Juan', email='juan@gmail.com')
     new_account.save()
