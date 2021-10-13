67dc840212e66ff1937dce746a9351a8a1c720f7
samsax <samuelsaxofon@gmail.com>
2021-10-12 20:23:25 -0500

    set S3 to aws init

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ from function.aws_services import AwsServices
 from function.transaction import send_mail_summary, summary_transacction
 import os
 import logging
+from botocore.client import Config
+import boto3
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
+config = Config(connect_timeout=5, retries={'max_attempts': 0})
+s3 = boto3.client('s3', config=config)
 def lambda_handler(event, _):
     try:
         logger.info('## ENVIRONMENT VARIABLES')
         logger.info(os.environ)
         logger.info('## EVENT')
         logger.info(event)
-        awsServices = AwsServices(event)
+        awsServices = AwsServices(event,s3)
         awsFile = awsServices.get_s3_file()
         logger.info(awsFile)
         new_person = get_or_create_account()
